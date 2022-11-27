9bbe72d8029f9ce37bca0658f2c3f1c922cedeb9
Stefan Haas <stefan.haas.privat@gmail.com>
2022-11-27 18:39:59 +0100

    service abuse

--- src/app/features/boarding/boarding-dashboard/boarding-dashboard.component.ts
+++ src/app/features/boarding/boarding-dashboard/boarding-dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BoardingService } from 'src/app/core/boarding.service';
+import { PassengerService } from 'src/app/core/passenger.service';
 
 @Component({
   selector: 'app-boarding-dashboard',
@@ -9,11 +10,14 @@ import { BoardingService } from 'src/app/core/boarding.service';
 export class BoardingDashboardComponent implements OnInit {
 
   constructor(
-    private boardingService: BoardingService
+    private boardingService: BoardingService,
+    // DIRTY:
+    private passengerService: PassengerService,
   ) { }
 
   ngOnInit(): void {
     this.boardingService.echo();
+    this.passengerService.echo();
   }
 
 }
